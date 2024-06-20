3301a23ff0d417c29dae92892e29ce8c0a5142c9
Jarllan1 <evayne8@gmail.com>
2024-06-20 12:05:27 +0800

    Activity 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,74 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main() => runApp(MaterialApp(
     home:Scaffold(
-      appBar: AppBar(
-       title: const Center (
-        child:  Text('Front App'),
-       ),
-        backgroundColor: Colors.teal[400],
+      backgroundColor: Colors.lime,
+     appBar: AppBar(
+      title: Text('User Profile'
+      style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
       ),
-      body:  Center(
-        child: Text(
-            'Ayos na kainan na',
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.blue[200],
-          ),
+      backgroundColor: Colors.limeAccent,
+       centerTitle: true,
+    ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'NAME:',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black87,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text(
+              'Jarllan C. Eleponga',
+              style: TextStyle(
+                fontSize:25.0 ,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'YEAR:',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black87,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text(
+              'Third Year',
+              style: TextStyle(
+                fontSize:25.0 ,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'Email:',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black87,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text(
+              'Evayne8@gmail.com',
+              style: TextStyle(
+                fontSize:25.0 ,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Icon(
-          Icons.add_moderator_outlined,
-          color: Colors.indigoAccent,
         ),
-          ),
-        ),
-      ),
-    );
-}
+      ));
+
 
