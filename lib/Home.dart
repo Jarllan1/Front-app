af908a04a4f4bec78a5f40e3eecd4fbd736f0912
jarllan1 <evayne8@gmail.com>
2024-06-25 12:09:25 +0800

    Add profile pic and buttons

---
+++ lib/Home.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lime,
+      appBar: AppBar(
+        title: Text(
+          'User Profile',
+          style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.limeAccent,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    child: Image.asset('assets/reindeer.png'),
+                    radius: 50.0,
+                  ),
+                ),
+                Divider(height: 75.0,color:Colors.lime ,),
+                Row(
+                  children: [
+                    Icon(
+                        Icons.person_2,
+                        color: Colors.amber,
+                    ),
+                    SizedBox(width: 3.0,),
+                    Text(
+                      'NAME:',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black87,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Jarllan C. Eleponga',
+                  style: TextStyle(
+                    fontSize:25.0 ,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Row(
+                  children: [
+                    Icon(Icons.calendar_month,
+                    color: Colors.amber,
+                    ),
+                    SizedBox(width: 3.0,),
+                    Text(
+                      'YEAR:',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black87,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Third Year',
+                  style: TextStyle(
+                    fontSize:25.0 ,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Row(
+                  children: [
+                    Icon(Icons.email_sharp,
+                    color: Colors.amber,
+                    ),
+                    SizedBox(width: 3.0,),
+                    Text(
+                      'Email:',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black87,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Evayne8@gmail.com',
+                  style: TextStyle(
+                    fontSize:25.0 ,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+              ],
+            ),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+
+              children: [
+                ElevatedButton(
+                   style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.amber)),
+                  onPressed: (){},
+                    child: Text(
+                        'Add Year',
+                    style: TextStyle(
+                      color: Colors.black87,
+
+                    ),
+                    ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:jarllan/Home.dart';
 
 void main() => runApp(MaterialApp(
-    home:Scaffold(
-      backgroundColor: Colors.lime,
-     appBar: AppBar(
-      title: Text(
-        'User Profile',
-      style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
-      ),
-      backgroundColor: Colors.limeAccent,
-       centerTitle: true,
-    ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'NAME:',
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Colors.black87,
-                letterSpacing: 2.0,
-              ),
-            ),
-            Text(
-              'Jarllan C. Eleponga',
-              style: TextStyle(
-                fontSize:25.0 ,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'YEAR:',
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Colors.black87,
-                letterSpacing: 2.0,
-              ),
-            ),
-            Text(
-              'Third Year',
-              style: TextStyle(
-                fontSize:25.0 ,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'Email:',
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Colors.black87,
-                letterSpacing: 2.0,
-              ),
-            ),
-            Text(
-              'Evayne8@gmail.com',
-              style: TextStyle(
-                fontSize:25.0 ,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
-              ),
-            ),
-          ],
-        ),
-      ),
-        ),
+    home:Home(),
       ));
-
 
